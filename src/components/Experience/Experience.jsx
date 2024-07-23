bc079e83ea08d16aafbac7a16210cb2fae107074
Zachary Polof <ZacharyPolof@gmail.com>
2024-07-22 19:11:36 -0500

    Experience Component
    Start of setup for experience component.
    
    Setup json files for skills, history, and projects for component

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './Experience.module.css';
+import skills from '../../data/skills.json';
+import history from '../../data/history.json';
+import { getImageUrl } from '../../utils';
+
+export const Experience = () => {
+    return (
+        
+    )
+};
